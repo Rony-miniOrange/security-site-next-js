ebe653e33a6d53b67c3fcc6aed8cf8f9423c9a0e
RonyAibloq-1503 <rohan.nikam@aibloq.com>
2023-03-21 16:22:39 +0530

    Minor updates in faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,11 +3,12 @@ import { GrAdd } from "react-icons/gr";
 import { AiOutlineMinus } from "react-icons/ai";
 const FaqSection = (props) => {
     const [faqs, setFaqs] = useState(null);
-    const [activeTab, setActivetab] = useState("tab1");
+    const [activeTab, setActivetab] = useState(0);
     const [open, setOpen] = useState(null);
 
     useEffect(() => {
-        setFaqs(props.faqs)
+        console.log(props.faqs);
+        setFaqs(props.faqs);
     }, []);
 
     const toggle = (index) => {
@@ -22,7 +23,7 @@ const FaqSection = (props) => {
     const handleTab = (id) => {
         setActivetab(id);
     };
-    
+
     return (
         <div className="container-fluid my-5 px-5">
             <div className="row d-flex">
@@ -31,62 +32,49 @@ const FaqSection = (props) => {
                 </div>
                 <div className="col-md-4 d-flex px-5">
                     {
-                        faqs && faqs.map((tab) => (
-                            <div className={"border rounded-1 col-md-4 mx-2 d-flex justify-content-center " + (activeTab == tab.id ? "bg-dark" : "bg-white")}>
-                                <ul key={tab.id} className="nav mx-2 justify-content-center align-self-center">
-                                    <li key={tab.id} className={activeTab === tab.id ? "active" : ""} onClick={() => handleTab(tab.id)}>
-                                        <a className={"nav-link " + (activeTab == tab.id ? "text-white" : "text-muted")} href="#" id={tab.id}>{tab.name}</a>
-                                    </li>
-                                </ul>
+                        faqs && faqs.map((tab,index) => (
+                            <div role="button" key={`faq-section-tab-${tab.id}`} className={"border rounded-1 col-md-4 mx-2 d-flex justify-content-center align-items-center " + (activeTab === index ? "bg-dark" : "bg-white")} onClick={() => handleTab(index)}>
+                                <a className={"nav-link " + (activeTab == index ? "text-white" : "text-muted")} id={tab.id}>{tab.name}</a>
                             </div>
                         ))
                     }
                 </div>
             </div>
             <div className="border rounded-3 my-4 w-100">
-                {
-                    faqs && faqs.map((tab) => (
-                        activeTab === tab.id &&
-                        //   <FaqSection tabSection={tab.data} />
-                        <div>
-                            {
-                                tab && tab.data.map((faq, index) => {
-                                    return (
-                                        <div className="py-4 mx-5 border-bottom ">
-                                            <div className="heading d-flex ">
-                                                <div className="title w-75 ">
-                                                    <h5 className={"font-weight-normal heading " + (open == index ? "text-primary" : "text")}>{faq.title}</h5>
-                                                </div>
-                                                <div className="expand w-25 d-flex justify-content-end">
-                                                    <button className="btn toggle" onClick={() => toggle(index)}>
-                                                        {
-                                                            open !== null && open == index &&
-                                                            <AiOutlineMinus className="fs-5" />
-                                                        }
-                                                        {
-                                                            open !== null && open !== index &&
-                                                            <GrAdd className="fs-5" />
-                                                        }
-                                                        {
-                                                            open == null &&
-                                                            <GrAdd className="fs-5" />
-                                                        }
-                                                    </button>
-                                                </div>
-                                            </div>
+                {faqs && faqs[activeTab]?.data.map((faq, index) => {
+                        return (
+                            <div className="py-4 mx-5 border-bottom ">
+                                <div className="heading d-flex ">
+                                    <div className="title w-75 ">
+                                        <h5 className={"font-weight-normal heading " + (open == index ? "text-primary" : "text")}>{faq.title}</h5>
+                                    </div>
+                                    <div className="expand w-25 d-flex justify-content-end">
+                                        <button className="btn toggle" onClick={() => toggle(index)}>
                                             {
-                                                open == index && <div className="description w-75">
-                                                    <p>{faq.solution}</p>
-                                                </div>
+                                                open !== null && open == index &&
+                                                <AiOutlineMinus className="fs-5" />
                                             }
-                                        </div>
-                                    )
+                                            {
+                                                open !== null && open !== index &&
+                                                <GrAdd className="fs-5" />
+                                            }
+                                            {
+                                                open == null &&
+                                                <GrAdd className="fs-5" />
+                                            }
+                                        </button>
+                                    </div>
+                                </div>
+                                {
+                                    open == index && <div className="description w-75">
+                                        <p>{faq.solution}</p>
+                                    </div>
                                 }
-                                )
-                            }
-                        </div>
-                    ))
+                            </div>
+                        )
+                    })
                 }
+
             </div>
         </div>
     )
