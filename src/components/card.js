57ddd67387ddd6a710caebb9172e23a90ffe8ef4
Shruti Jain <shruti.jain@xecurify.com>
2023-03-21 17:44:03 +0530

    Image component added

--- src/components/card.js
+++ src/components/card.js
@@ -1,42 +1,43 @@
+import icon from '/public/UniqueSolutionImages/icon.png';
 export default 
 [
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Single Sign On",
         text: "Our support engineers are available for query resolution 24 X 7. Just drop us an email at 2fasupport@xecurify.com",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Protect Complete Site",
         text: " miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "SCIM User Profile Sync",
         text: "miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Attribute and Role Mapping",
         text: "miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Multisite Network Support",
         text: " miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Multiple IDP Support",
         text: "miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "SSO for Admin Pages",
         text: "miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
     {
-        img: "/images/icon.webp",
+        img: icon.src,
         title: "Widget for SSO Login",
         text: " miniOrange is a domain expert in IAM products such as SSO, 2FA, MFA as well as customised enterprise solutions.",
     },
